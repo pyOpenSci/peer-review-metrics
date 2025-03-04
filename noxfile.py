3650e47f23db6a6e136154db1908904c91d1be88
Leah Wasser <leah@pyopensci.org>
2025-03-03 19:42:57 -0700

    fix: don't install quarto twice in CI

--- noxfile.py
+++ noxfile.py
@@ -7,6 +7,7 @@ nox.options.reuse_existing_virtualenvs = True
 def html(session):
     """Builds the html for the site locally for static rendering."""
     session.install("-r", "requirements.txt")
+    session.install("quarto-cli")
     session.install("-e", "./pyosmetrics_pkg")
     cmd = ["quarto", "render"]
     session.run(*cmd)
@@ -17,6 +18,7 @@ def serve(session):
     """Builds the quarto site locally using quarto preview. Opens a local host
     which allows for a live preview that updates as you work."""
     session.install("-r", "requirements.txt")
+    session.install("quarto-cli")
     session.install("-e", "./pyosmetrics_pkg")
     cmd = ["quarto", "preview"]
     session.run(*cmd)
